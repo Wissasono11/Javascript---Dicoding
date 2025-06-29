6c94908b41c721f5063fb5183f5d8837e669d8ab
Wissasono11 <bayuwicaksono782@gmail.com>
2025-06-29 18:27:52 +0700

    feat:menambahkan baris komentar dalam es6

---
+++ Dunia OOP/encapsulation.js
@@ -0,0 +1,1 @@
+// encapsulation adalah proses untuk menyembunyikan data dan hanya memberikan akses melalui method tertentu
--- Dunia OOP/es6.js
+++ Dunia OOP/es6.js
@@ -2,11 +2,14 @@
 // class adalah template untuk membuat object
 
 class Person {
+    // ini adalah method constructor
     constructor(name, age) {
+        // ini adalah property dari class Person
         this.name = name;
         this.age = age;
     }
-
+    
+    // ini adalah method eat
     eat() {
         console.log(`${this.name} is eating`);
     }
@@ -16,8 +19,10 @@ class Person {
 const person1 = new Person("John", 30);
 const person2 = new Person("Jane", 25);
 
+// ini cara mengakses property dan method dari object
 console.log(person1.name); // John
 console.log(person2.name); // Jane
 
+// ini cara mengakses property dan method dari object
 person1.eat(); // John is eating
 person2.eat(); // Jane is eating
