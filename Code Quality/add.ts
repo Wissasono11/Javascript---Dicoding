22a607345e77dcfd692c179735cba66d91fe1be4
Wissasono11 <bayuwicaksono782@gmail.com>
2025-07-07 11:03:55 +0700

    feat: bermain-main dengan typescript dan bun

---
+++ Code Quality/add.ts
@@ -0,0 +1,6 @@
+function add(numA: number, numB: number): number {
+    return numA + numB;
+}
+
+const result = add(5, 3);   
+console.log(result);
---
+++ Code Quality/greet.ts
@@ -0,0 +1,9 @@
+//  penggunaan typescript
+
+const myName: string = "Asep Verstappen";
+
+function greet(name: string){
+    console.log(`Hello ${name}. My name is ${myName}!`);
+}
+
+greet('John Doe');
