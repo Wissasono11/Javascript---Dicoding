5459ad43074c1b78e27bce16c2bfa8dce7177441
Wissasono11 <bayuwicaksono782@gmail.com>
2025-07-07 10:40:53 +0700

    feat:mengenal tentang code quality seperti jsdoc dan flow

---
+++ Code Quality/flow.js
@@ -0,0 +1,22 @@
+// flow adalah library JavaScript yang digunakan untuk memeriksa tipe data dalam kode JavaScript.
+// Dengan menggunakan flow, Anda dapat menambahkan anotasi tipe data untuk memastikan bahwa kode Anda sesuai
+
+// contoh penggunaan flow untuk memeriksa tipe data dalam kode JavaScript
+// dan menghindari kesalahan tipe data yang umum terjadi:
+
+// const myName: string = "Asep Verstappen";
+
+// function greet(name: string) {
+//     console.log(`Hello ${name}. My name is ${myName}!`);
+// }
+
+// greet('John Doe');
+
+
+const myName = "Asep Verstappen";
+
+function greet(name) {
+    console.log(`Hello ${name}. My name is ${myName}!`);
+}
+
+greet('John Doe');
---
+++ Code Quality/jsDoc.js
@@ -0,0 +1,20 @@
+// JSDoc adalah alat untuk membuat dokumentasi kode JavaScript.
+// Dengan JSDoc, Anda dapat menambahkan komentar khusus di dalam kode Anda.
+
+/** Ini adalah contoh sederhana dari JSDoc */
+function greeting(name){
+    console.log("Hello, " + name + "!");
+}
+
+/**
+ * Ini adalah contoh fungsi yang menggunakan JSDoc yang lebih kompleks 
+ * Get add operation 
+ * 
+ * @param {number} numA - The first number operand
+ * @param {number} numB - The second number operand
+ * @return {number} The sum of numA and numB
+ */
+
+function add(numA, numB) {
+    return numA + numB
+}
